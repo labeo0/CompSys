31261056bdcf88d0ee6b20f2097eefb898e8adfa
labeo0 <33054098+labeo0@users.noreply.github.com>
2018-03-27 12:45:21 +0300

    Add files via upload

--- Lab1/Lab1.1.cs
+++ Lab1/Lab1.1.cs
@@ -14,11 +14,11 @@ namespace Lab1._1CS_cs
         static void Main(string[] args)
         {
             double H = 0;//entropy
-            int info = 0;//info quantity
+            //int info = 0;//info quantity
             //string path = @"D:\1LABS\course3.2\LabComputerSystems\Lab1.1CS_cs\txt\13Base64.txt";
             //string path = @"D:\1LABS\course3.2\LabComputerSystems\Lab1.1CS_cs\txt\RepkaBase64.txt";
             //string path = @"D:\1LABS\course3.2\LabComputerSystems\Lab1.1CS_cs\txt\GPLv2Base64.txt";
-            string path = @"D:\1LABS\course3.2\LabComputerSystems\Lab1.1CS_cs\txt\GPLv2Base64.tar.bz";
+            string path = @"D:\1LABS\course3.2\LabComputerSystems\Lab1.1CS_cs\txt\GPLv2.tar.bz2";
             string text = "";
             double len = 0.0;
             Console.OutputEncoding = Encoding.UTF8;
@@ -30,7 +30,7 @@ namespace Lab1._1CS_cs
             //Console.WriteLine(text);
             len = text.Length;
             Dictionary<char, int> pair = new Dictionary<char, int>();
-            double CountSym = 0;
+
             foreach (char c in text)
             {
                 if (pair.ContainsKey(c))
@@ -38,15 +38,15 @@ namespace Lab1._1CS_cs
                 else
                 {
                     pair.Add(c, 1);
-                    CountSym++;
+                    
                 }
             }
             foreach (var item in pair)
             {
                 H -= (item.Value / len) * Math.Log(item.Value / len, 2);
-                Console.WriteLine("Symbol: " + item.Key + " Frequency: " + item.Value + " Probability: " + item.Value / len + " \n");
+                //Console.WriteLine("Symbol: " + item.Key + " Frequency: " + item.Value + " Probability: " + item.Value / len + " \n");
             }
-            Console.WriteLine("Entropy bit: " + H + "\n" + "Info quantity byte: " + H * CountSym / 8);
+            Console.WriteLine("Entropy bit: " + H + "\n" + "Info quantity byte: " + H * len / 8);
             FileInfo file = new FileInfo(path);
             Console.WriteLine("File name:  "+file.Name+" File size byte: "+ file.Length);
             Console.WriteLine(len);
